685b97b7b38ea233054c1c3ef62018376b7ac6e2
Tucker <tuckerbodell@gmail.com>
2025-01-18 13:39:31 -0700

    Fix

--- src/main/java/net/mcreator/tags/Launcher.java
+++ src/main/java/net/mcreator/tags/Launcher.java
@@ -19,20 +19,17 @@ public class Launcher extends JavaPlugin {
         addListener(MCreatorLoadedEvent.class, event -> SwingUtilities.invokeLater(() -> {
             MCreator mcreator = event.getMCreator();
             if (mcreator != null) { // Ensure MCreator is not null
-                // Add an action for opening the custom dialog
+                // Add an action for opening the custom Tags+ dialog (AddOtherTagsDialog)
                 BasicAction openTagsDialogAction = new BasicAction(
                         mcreator.getActionRegistry(),
                         L10N.t("menu.tags.open_dialog"), // Localized menu item name
-                        e -> {
-                            // Open your custom dialog
-                            JOptionPane.showMessageDialog(mcreator, "Tags+ dialog opened!");
-                        }
+                        e -> net.mcreator.tags.ui.dialogs.AddOtherTagsDialog.open(mcreator) // Open the AddOtherTagsDialog
                 );
                 openTagsDialogAction.setIcon(UIRES.get("16px.play")); // Example icon
 
                 // Create the "Tags+" menu
                 JMenu tagsMenu = new JMenu(L10N.t("menu.tags")); // Localized menu name
-                tagsMenu.add(openTagsDialogAction);
+                tagsMenu.add(openTagsDialogAction);  // Add AddOtherTagsDialog to the Tags menu
 
                 // Add the "Tags+" menu to the main menu bar
                 JMenuBar menuBar = mcreator.getMainMenuBar();
