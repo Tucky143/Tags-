d522111e1193ffe4a7d8483d7a419bfb4953e02e
Tucky143 <100064285@apps.ogdensd.org>
2025-02-02 15:21:20 -0700

    Update 2

--- src/main/java/net/mcreator/tags/Launcher.java
+++ src/main/java/net/mcreator/tags/Launcher.java
@@ -10,6 +10,9 @@ import net.mcreator.ui.init.UIRES;
 
 import javax.swing.*;
 
+import static com.github.weisj.jsvg.attributes.text.Side.Left;
+import static javax.swing.SwingConstants.LEFT;
+
 public class Launcher extends JavaPlugin {
 
     public Launcher(Plugin plugin) {
@@ -25,16 +28,13 @@ public class Launcher extends JavaPlugin {
                         L10N.t("menu.tags.open_dialog"), // Localized menu item name
                         e -> net.mcreator.tags.ui.dialogs.AddOtherTagsDialog.open(mcreator) // Open the AddOtherTagsDialog
                 );
-                openTagsDialogAction.setIcon(UIRES.get("16px.play")); // Example icon
-
-                // Create the "Tags+" menu
-                JMenu tagsMenu = new JMenu(L10N.t("menu.tags")); // Localized menu name
-                tagsMenu.add(openTagsDialogAction);  // Add AddOtherTagsDialog to the Tags menu
+                openTagsDialogAction.setIcon(UIRES.get("16px.injecttags"));
 
                 // Add the "Tags+" menu to the main menu bar
                 JMenuBar menuBar = mcreator.getMainMenuBar();
                 if (menuBar != null) {
-                    menuBar.add(tagsMenu);
+                    mcreator.getToolBar().addToLeftToolbar(openTagsDialogAction);
+                    openTagsDialogAction.setTooltip("Add Additional Tags...");
                     menuBar.revalidate();
                     menuBar.repaint();
                 }
